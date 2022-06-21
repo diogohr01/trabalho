46eef5cb30ea39279d4b24881ab26eccf08872dc
Diogo Rodrigues <diogorodrigues699@yahoo.com>
2022-06-21 20:00:27 -0300

    first commit

--- trabalho.js
+++ trabalho.js
@@ -1,59 +1,94 @@
-function loopDePares(num){
+const pessoa = [
+   {
+    altura: 1.78,
+    sexo: "M"
+   },
+   {
+    altura: 1.55,
+    sexo: "M"
+   },
+   {
+    altura: 1.48,
+    sexo:"M"
+   },
+   {
+    altura: 1.72,
+    sexo: "M"
+   },
+   {
+    altura: 1.83,
+    sexo: "F"
+   },
+   {
+    altura: 1.65,
+    sexo:"F"
+   },
+   {
+    altura: 1.74,
+    sexo: "M"
+   },
+   {
+    altura: 1.68,
+    sexo:"F"
+   },
+   {
+    altura: 1.77,
+    sexo: "M"
+   },
+   {
+    altura: 1.59,
+    sexo:"F"
+   },
+   {
+    altura: 1.81,
+    sexo: "M"
+   },
+   {
+    altura: 1.87,
+    sexo: "M"
+   },
+   {
+    altura: 1.55,
+    sexo: "F"
+   },
+   {
+    altura: 1.60,
+    sexo: "M"
+   },
+   {
+    altura: 1.66,
+    sexo: "F"
+   }
+];
 
-    for( let i = 0;  i  <= 100; i++){
-      console.log(i)
-      if((num + i) % 2 == 0){
-        console.log( 'O número', i,'é par')
-      }
-    } 
-}
 
-console.log(loopDePares(100))
 
-//Questão 2
+const min = pessoa.reduce(function(a,b){
+    return a.altura < b.altura ? a : b
+});
+ console.log (min)
 
-function loopDeImpares(numero, palavra){
-    for( let i = 0;  i  <= 100; i++){
-      console.log(i)
-      if((numero + i) % 2 != 0){
-        console.log(palavra)
-      }
-    } 
-  }
+const max = pessoa.reduce(function(a,b){
+    return a.altura > b.altura ? a : b
+});
+ console.log (max);
 
-  console.log(loopDeImpares(1,'banana'))
 
-//Questão 03
+let alturaMulheres = 0;
+let contador = 0;
+ const mulheres = pessoa.filter(function(p){
+ if(p.sexo === "F"){
+    alturaMulheres += p.altura;
+    contador++;}
+});
+ console.log(alturaMulheres/contador);
 
-function soma(num){
-    let somador = 0;
-    for(let i = 0; i<=num; i++){
-      somador += i;
-    }
-    return somador
-  }
-console.log(soma(100));
+const homens = pessoa.filter(pessoa => pessoa.sexo === "M").length
+ console.log(homens);
 
-//Questão 04
 
-function newArray(num){
-    let lista = [];
-    for(let i = 1; i<=num; i++){
-      lista.push(i);
-    }
-    return lista
-  }
+ 
 
-  console.log(newArray(10));
 
-//Questão 05
 
-function split(palavra){
-    lista = [];
-    for(let i = 0; i < palavra.length; i++){
-      lista.push(palavra[i]);
-    }
-    return lista
-  }
-  
-console.log(split('olá'))
+
